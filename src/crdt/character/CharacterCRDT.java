36c7ab78b80e728f2783d917b6825959dee0e121
MohamedShalaby <Mohamed.Shalaby04@eng-st.cu.edu.eg>
2026-04-01 01:38:54 +0200

    Characters insertion in tree

--- src/crdt/character/CharacterCRDT.java
+++ src/crdt/character/CharacterCRDT.java
@@ -1,4 +1,33 @@
 package crdt.character;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CharacterCRDT {
+
+    Map<String, CRDTNode> nodeMap;
+    List<CRDTNode> roots; //nodes with parent = null
+
+    public CharacterCRDT() {
+        nodeMap = new HashMap<>();
+        roots = new ArrayList<>();
+    }
+
+    public void insert(String id, char value, String parentId) {
+        CRDTNode parent = null;
+        if (parentId != null) {
+            parent = nodeMap.get(parentId);
+        }
+        CRDTNode newNode = new CRDTNode(id, value, parent);
+
+        if (parent == null) {
+            roots.add(newNode);
+        } else {
+            parent.children.add(newNode);
+        }
+
+        nodeMap.put(id, newNode);
+    }
 }
